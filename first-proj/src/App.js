e8e590eefcb30da7679afc9137c8afbb4de87cb0
adityasingh0912 <singh0912aditya@gmail.com>
2024-06-19 12:18:32 +0530

    1.336

--- first-proj/src/App.js
+++ first-proj/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import dotenv from 'doten';
+dotenv.config()
 function App() {
   const [numberid, setnumberid] = useState("");
   const [numbers, setNumbers] = useState([]);
   const [average, setAverage] = useState(0);
   const url = "http://20.244.56.144/numbers/{numberid}";
+  const token = process.env.token
 
   const handleInputChange = (e) => {
     setnumberid(e.target.value);
@@ -17,7 +19,7 @@ function App() {
       try {
         const response = await axios.get(url.replace("{numberid}", numberid), {
           headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE4Nzc0NDIzLCJpYXQiOjE3MTg3NzQxMjMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImIwZjIyZjdjLTRhMTEtNDgyZi04NzBhLWRiNGI0MTUzMDIwYiIsInN1YiI6IjIxMDMwNTEyNDE1NEBwYXJ1bHVuaXZlcnNpdHkuYWMuaW4ifSwiY29tcGFueU5hbWUiOiJBZmZvcmRtZWQgVGVjaG5vbG9naWVzIiwiY2xpZW50SUQiOiJiMGYyMmY3Yy00YTExLTQ4MmYtODcwYS1kYjRiNDE1MzAyMGIiLCJjbGllbnRTZWNyZXQiOiJESE52ZUlrb1JEdERkWXNYIiwib3duZXJOYW1lIjoiQWRpdHlhIiwib3duZXJFbWFpbCI6IjIxMDMwNTEyNDE1NEBwYXJ1bHVuaXZlcnNpdHkuYWMuaW4iLCJyb2xsTm8iOiIyMTAzMDUxMjQxNTQifQ.hX5kN_TQ-2cB2KXzf7_Ci6fD4WdqECrdhMuyn-1a2FA',
+            'Authorization': `Beare ${token}`,
             'Content-Type': 'application/json'
           }
         });
